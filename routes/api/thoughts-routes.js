c341b69c905cde281afed3dcd34821f644bab459
Derick Hooley <dghooley@gmail.com>
2021-12-12 10:21:40 -0600

    builtRoutes

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction 
+} = require('../../controllers/thoughts-controller');
+
+router 
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThoughts);
+
+router
+    .route('/:id')
+    .get(getThoughtsById)
+    .put(updateThoughts)
+    .delete(deleteThoughts);
+
+router.route('/:thoughtsId/reactions/')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
